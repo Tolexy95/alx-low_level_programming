6d994bf08f91be66008d06a0773e40bcab2ed24d
Tolexy95 <toyin95.olubayo@gmail.com>
2022-03-21 10:47:50 -0700

    Task 101 corrected

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,15 @@
-#import "main.h"
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main -
- * Return:
  */
 int main(void)
 {
-	return (0);
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 }
