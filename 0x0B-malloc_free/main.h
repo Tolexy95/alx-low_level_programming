8dff7c7c385285b3ce6e33f5bb65ce68cbf8a4da
Tolexy95 <toyin95.olubayo@gmail.com>
2022-04-05 08:02:42 -0700

    Task 1 redo

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,6 @@
-
-int _putchar(char c);  /* An example function declaration */
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
