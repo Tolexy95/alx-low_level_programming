477687f35bad7a80c58d1db290b3224494e58c5c
Tolexy95 <toyin95.olubayo@gmail.com>
2022-04-13 11:31:08 -0700

    Task 100 redo

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ *
+ * Return: 0 on succes, 1 on argv != 2, 2 on negative bytes in argv
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int bytes, i;
-	unsigned char *func_ptr;
+	int i, bytes;
+	char *mainaddr;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+		printf("Error\n"), exit(1);
+
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
+		printf("Error\n"), exit(2);
+
+	mainaddr = (char *)main;
+
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", mainaddr[i]);
+
+	printf("%02hhx\n", mainaddr[i]);
+
 	return (0);
 }
